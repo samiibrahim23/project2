603581c6b8db3c2bf4348d0b51f887c1a542f6ae
guettaya <guettayaabdou@gmail.com>
2025-05-19 07:40:05 +0100

    Update bidirectionalLinkedLists.h

--- bidirectionalLinkedLists.h
+++ bidirectionalLinkedLists.h
@@ -2,396 +2,333 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <time.h>
 #include<string.h>
-
-
-
-typedef struct logentry{
-	struct logentry *prv;
-	int id,severity;
-	char message[50],date[20];
-	struct logentry *next;
-	}logentry;
-	
-	
-void InsertLogEntry(logentry **head,logentry **tail ,int ID,int SEVERITY ,char MESSAGE[256],int where ){
-logentry 	*temp,*temp1=*head,*temp2=*head;
-int count=0;
-time_t t=time(NULL);
-    struct tm datetime=*localtime(&t);
-    temp=malloc(sizeof(logentry));
-	temp->id=ID;
-	temp->severity=SEVERITY;
-	strcpy(temp->message,MESSAGE);
-	strftime(temp->date,sizeof(temp->date),"%Y-%m-%d %H:%M",&datetime);
-	if(*head==NULL){
-		*head=temp;
-		temp->next=NULL;
-		temp->prv=NULL;
-		*tail=temp;
-		return;
-	}
-	if(where==0){
-	temp->prv=NULL;
-	temp->next=*head;
-	*head=temp;}
-	if(where==-1){
-	(*tail)->next=temp;
-	temp->prv=*tail;
-	temp->next=NULL;
-	*tail=temp;}
-	if(where!=-1 && where!=0){
-	 while(temp1->next!=NULL && count!=where){
-	 	temp2=temp1;
-	 	temp1=temp1->next;
-		 count++;}
-		 if(count==where){
-		 	temp2->next=temp;
-		 	temp->next=temp1;
-		 	temp->prv=temp2;
-		 	temp1->prv=temp;
-		 }
-		 else{
-	    (*tail)->next=temp;
-	    temp->prv=*tail;
-      	temp->next=NULL;
-	    *tail=temp;}}}
-void Delete_Log_Entry_ID(logentry **head,logentry **tail,int ID){//  if the id dont exist delete the last one  
-	logentry *temp=*head,*temp1=*tail,*temp0;
-	while(temp->id!=ID && temp1->id!=ID && temp->prv!=temp1){
-		temp=temp->next;
-		temp1=temp1->prv;}
-		if(temp->id==ID){
-			if(temp==*head){
-				temp0=temp->next;
-				temp0->prv=NULL;
-				*head=temp0;
-				free(temp);}
-			else{
-			temp0=temp->prv;
-			temp1=temp->next;
-			temp0->next=temp1;
-			temp1->prv=temp0;
-			free(temp);
-			return;}}
-			if(temp1->id==ID){
-				if(temp1==*tail){
-					temp0=temp1->prv;
-				    temp0->next=NULL;
-				    *tail=temp0;
-				free(temp1);}
-			else{
-			temp0=temp1->prv;
-			temp=temp1->next;
-			temp0->next=temp;
-			temp->prv=temp0;
-			free(temp1);}}
-			if(temp1==temp->prv){
-				temp0=(*tail)->prv;
-				temp0->next=NULL;
-				free(*tail);
-				*tail=temp0;}}
-logentry* serch_by_id(logentry *head,logentry *tail,int id){
-	 
-	 
-	 while(tail->id!=id && head->id!=id) {
-	 	if(tail==head | tail->next==head)
-	 	return(NULL);
-	 	tail=tail->prv;
-	 	head=head->next;
-	 }
-	 if(tail->id==id)
-	 return(tail);
-	 else
-	 return(head);
+#define SIZE 100
+typedef struct node {
+    char word[50];
+    char synonym[50];
+    char antonym[50];
+    int numChars[3];
+    int numVowels[3];
+    struct node* next;
+} node;
+ typedef node *TList ;
+typedef struct stack{
+    TList top;
+}stack;
+typedef stack* TStack;
+TStack createstack(){
+    TStack stack1=malloc(sizeof(stack));
+    stack1->top=NULL;
+    return stack1;
 }
-logentry* serch_by_date(logentry *head,logentry *tail,char date[25]){// write the date like this %Y-%m-%d %H:%M
-      while(strcmp(date,head->date)!=0){
-      	if(head==NULL)
-      	return(NULL);
-      	head=head->next;}
-		  return(head);}
-logentry* serch_by_keyword(logentry *head,logentry *tail,char keyword[25]){
-	while(strstr(head->message,keyword)!=NULL){
-		if(head==NULL)
-		return(NULL);
-		head=head->next;}
-		return(head);
+bool isempty(TStack stk){
+    if(stk->top==NULL)
+    return true ;
+    return false;
 }
-void sort_log_by_severity(logentry **head,logentry **tail){
-	logentry *temp=*head,*mid=NULL,*temp2=NULL,*temp3=NULL;
-	int check=1;
-	char date[20];
-	while(check!=0){
-		check=0;
-		temp=*head;
-		while(temp->next!=NULL){
-			mid=temp->next;
-		if(temp->severity<=mid->severity){
-		temp=temp->next;
-		mid=temp->next;}
-		else{
-			check=1;
-			if(temp->prv==NULL){
-				temp2=mid->next;
-				*head=mid;
-				mid->prv=NULL;
-				mid->next=temp;
-				temp->prv=mid;
-				temp->next=temp2;
-				temp2->prv=temp;
-			}
-			else{
-				if(mid->next==NULL){
-					temp2=temp->prv;
-					temp2->next=mid;
-					mid->prv=temp2;
-					mid->next=temp;
-					temp->prv=mid;
-					temp->next=NULL;
-					*tail=temp;
-					
-				}
-				else{
-		
-			temp3=temp->prv;
-			temp2=mid->next;
-			temp->next=temp2;
-			temp->prv=mid;
-			mid->next=temp;
-			temp2->prv=temp;
-			temp3->next=mid;
-			}}}}
-			temp=*head;}}
-void sort_log_by_date(logentry **head,logentry **tail){
-	logentry *temp=*head,*mid=NULL,*temp2=NULL,*temp3=NULL;
-	int check=2;
-	char date[20];
-	while(check!=0){
-		check=0;
-		strcpy(date,temp->date);
-		while(temp->next!=NULL){
-			mid=temp->next;
-		if(strcmp(date,mid->date)<=0){
-		temp=temp->next;
-		mid=temp->next;
-		strcpy(date,temp->date);
-		}
-		else{
-			check=1;
-			if(temp->prv==NULL){
-				temp2=mid->next;
-				*head=mid;
-				mid->prv=NULL;
-				mid->next=temp;
-				temp->prv=mid;
-				temp->next=temp2;
-				temp2->prv=temp;
-			}
-			else{
-				if(mid->next==NULL){
-					temp2=temp->prv;
-					temp2->next=mid;
-					mid->prv=temp2;
-					mid->next=temp;
-					temp->prv=mid;
-					temp->next=NULL;
-					*tail=temp;
-					
-				}
-				else{
-		
-			temp3=temp->prv;
-			temp2=mid->next;
-			temp->next=temp2;
-			temp->prv=mid;
-			mid->next=temp;
-			temp2->prv=temp;
-			temp3->next=mid;
-			}}}}
-			temp=*head;}}
-int CountTotalLogs(logentry *left,logentry *right){ 
-    int count=1;
- while(left!=right){
-    count++;
-    left=left->next;
- }
- return count;
-}
-
-void reverse(logentry **head,logentry **tail){
-  logentry *left,*temp=*head,*right,*temp2,*left1;
-  while(temp2!=NULL){
-  	if((*head)->prv==NULL){
-  		(*head)->prv=(*head)->next;
-	    (*head)->next=NULL;
-		  right=(*head)->prv;
-		  right=right->next;
-		  temp=right;
-        left=(*head)->prv;
-        left1=*head;
-		temp2=temp->next;}
-	    else{
-
-	  right->next=left;
-	  left->prv=temp;
-	  left->next=left1;
-	  left1=left;
-	  left=right;
-	  right=temp2;
-	  temp2=temp2->next;}}
-	  left->prv=*tail;
-	  left->next=left1;
-	  (*tail)->next=left;
-	  (*tail)->prv=NULL;
-	  temp=*head;
-	  *head=*tail;
-	  *tail=temp;
-}
-void DeleteLogAtIndex(logentry **head, logentry **tail, int index) {
-    if (*head == NULL) return;
-    logentry *temp = *head;
-    int count = 0;
-    while (temp != NULL && count < index) {
-        temp = temp->next;
-      count++;
-    }
-    if (temp == NULL) return; 
-    if (temp == *head) {
-        *head = temp->next;
-        if (*head != NULL) (*head)->prv = NULL;
-        else *tail = NULL;
-    }
-    else if (temp == *tail) {
-        *tail = temp->prv;
-        (*tail)->next = NULL;
-    }
-
-    else {
-        temp->prv->next = temp->next;
-        temp->next->prv = temp->prv;
-}
-    free(temp);}
-void MergeLogLists(logentry **head1, logentry **tail1, logentry *head2) {
-    if (*head1 == NULL) {
-        *head1 = head2;
-        *tail1 = head2;
-        while (*tail1 && (*tail1)->next != NULL)
-            *tail1 = (*tail1)->next;
+void push(TStack stk,TList node){
+    if(isempty(stk)){
+        stk->top=copyNode(node);
         return;
     }
-    if (head2 == NULL) return; 
-    (*tail1)->next = head2;
-    head2->prv = *tail1;
-    while (head2->next != NULL)
-        head2 = head2->next;
-    *tail1 = head2;
+    TList temp=copyNode(node);
+    temp->next=stk->top;
+    stk->top=temp;
+    return ;
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-logentry* createLogEntry(int id, int severity, const char *message, const char *date) {
-    logentry *newLog = (logentry*)malloc(sizeof(logentry));
-    if (newLog == NULL) {
+TList pop(TStack stk){
+	TList popednode;
+    if(!isempty(stk)){
+        popednode=copyNode(stk->top);
+        stk->top=stk->top->next;
+        return popednode;
+    }
+    return NULL;}
+TList peek(TStack stk) {
+    if (!isempty(stk)) {
+        return stk->top;  
+    }
+    return NULL;
+}
+TList copyNode(TList src) {
+    TList newNode = malloc(sizeof(node));
+    if (newNode == NULL) {
         printf("Memory allocation failed!\n");
         return NULL;
     }
-    newLog->id = id;
-    newLog->severity = severity;
-    strncpy(newLog->message, message, sizeof(newLog->message) - 1);
-    newLog->message[sizeof(newLog->message) - 1] = '\0';  // Ensure null termination
-    strncpy(newLog->date, date, sizeof(newLog->date) - 1);
-    newLog->date[sizeof(newLog->date) - 1] = '\0';  // Ensure null termination
-    newLog->prv = NULL;
-    newLog->next = NULL;
-    return newLog;
+    strcpy(newNode->word, src->word);
+    strcpy(newNode->synonym, src->synonym);
+    strcpy(newNode->antonym, src->antonym);
+    for (int i = 0; i < 3; i++) {
+        newNode->numChars[i] = src->numChars[i];
+        newNode->numVowels[i] = src->numVowels[i];
+    }
+    newNode->next = NULL;
+    return newNode;
+}
+ typedef node *TList ;
+typedef struct TTree{
+	char word[50];
+    char synonym[50];
+    char antonym[50];
+    int numChars[3];
+    int numVowels[3];
+ struct TTree* left;
+struct TTree* right;
+}TTree;
+TList sortWord(TList syn){
+	TList first,secound;
+	char temp[100];
+	bool isSorted=false;
+	while(!isSorted){
+		first=syn;
+		secound=first->next;
+		isSorted=true;
+		while(secound!=NULL){
+			if(strcasecmp(first->word,secound->word)>0){
+				strcpy(temp,first->word);
+				strcpy(first->word,secound->word);
+				strcpy(secound->word,temp);
+				isSorted=false;}
+				else{
+					first=secound;
+					secound=secound->next;}}}
+					return(syn);}	  
+TTree *getInfWordTree(TTree *tr, char *word){
+	TTree *found;
+	if(tr==NULL)
+	return(NULL);
+	if(strcmp(tr->word,word)==0)
+	return(tr);
+	found=getInfWordTree(tr->left, word);
+	 if (found != NULL)
+        return found;
+
+    return getInfWordTree(tr->right, word);
+
+}
+TTree* toTree(TStack *stk) {
+    TTree *root = NULL;
+    char *word;
+    while (stk != NULL) {
+        word = pop(stk); 
+        root = insert(root, word);
+        free(word);}
+    return root;}
+
+TTree *UpdateWordBST(TTree *tr, char *word, char *syne, char *anton){
+	TTree *found;
+	if(tr==NULL)
+	return(NULL);
+	if(strcmp(tr->word,word)==0){
+		strcpy(tr->synonym,syne);
+		strcpy(tr->antonym,anton);
+	return(tr);}
+	found=UpdateWordBST(tr->left,word,syne,anton);
+	 if (found != NULL)
+        return found;
+
+    return UpdateWordBST(tr->right, word,syne,anton);
+}
+TTree* findMin(TTree* node) {
+    while (node && node->left != NULL)
+        node = node->left;
+    return node;
+}
+TTree* deleteWordBST(TTree* tr, char* word) {
+    if (tr == NULL)
+        return NULL;
+    int cmp = strcasecmp(word, tr->word);
+    if (cmp < 0) {
+        tr->left = deleteWordBST(tr->left, word);
+    } else if (cmp > 0) {
+        tr->right = deleteWordBST(tr->right, word);
+    } else {
+        if (tr->left == NULL && tr->right == NULL) {
+            free(tr);
+            return NULL;
+        }
+        if (tr->left == NULL) {
+            TTree* temp = tr->right;
+            free(tr);
+            return temp;
+        } else if (tr->right == NULL) {
+            TTree* temp = tr->left;
+            free(tr);
+            return temp;
+        }
+        TTree* minNode = findMin(tr->right);
+        strcpy(tr->word, minNode->word);
+        tr->right = deleteWordBST(tr->right, minNode->word); 
+    }
+
+    return tr;
+}
+TTree *AddWordBSTT(TTree *tr, char *word, char *syne, char *anton){
+    if (tr == NULL) {
+        TTree* node = (TTree*)malloc(sizeof(TTree));
+        strcpy(node->word, word);
+        strcpy(node->synonym,syne);
+        strcpy(node->antonym,anton);
+        node->left = node->right = NULL;
+        return node;
+    }
+    if (strcasecmp(word, tr->word) < 0)
+        tr->left =AddWordBSTT(tr->left, word,syne,anton);
+    else
+        tr->right =AddWordBSTT(tr->right, word,syne,anton);
+    return tr;}
+TTree* fillTree(FILE *f) {
+ TTree *root = NULL;
+    char word[50];
+    while (fscanf(f, "%s", word) != EOF) {
+        root = insert(root, word);}
+    return root;}
+
+TTree *TraversalBSTinOrder(TTree *tr){
+	if(tr==NULL)
+	return(NULL);
+	TraversalBSTinOrder(tr->left);
+	printf("%s",tr->word);
+	TraversalBSTinOrder(tr->right);}
+TTree *TraversalBSTpreOrder(TTree *tr){
+	printf("%s",tr->word);
+	TraversalBSTinOrder(tr->left);
+	TraversalBSTinOrder(tr->right);
+}
+TTree *TraversalBSTpostOrder(TTree *tr){
+	TraversalBSTpostOrder(tr->left);
+	TraversalBSTpostOrder(tr->right);
+	printf("%s",tr->word);}
+int Height(TTree *tr) {
+    if (tr == NULL) {
+        return 0;
+    } else {
+        int leftHeight = Height(tr->left);
+        int rightHeight = Height(tr->right);
+        
+        if (leftHeight > rightHeight) {
+            return 1 + leftHeight;
+        } else {
+            return 1 + rightHeight;
+        }}}
+int size(TTree *tr){
+	if (tr==NULL)
+	return(0);
+	return(1+size(tr->left)+size(tr->right));
+}
+void HighSizeBST(TTree *tr){
+	printf("the size of this tree is: %d",size(tr));
+	printf("the hight of this tree is: %d",Height(tr));
+}
+TTree* BSTMirror(TTree* tr) {
+    if (tr == NULL) return NULL;
+    // here i a Allocate a new node
+    TTree* mirrored = (TTree*)malloc(sizeof(TTree));
+    if (mirrored == NULL)
+	return NULL; // 
+    strcpy(mirrored->word,tr->word);
+    mirrored->left = BSTMirror(tr->right);
+    mirrored->right = BSTMirror(tr->left);
+
+    return mirrored;
+}
+TTree* LowestCommonAncestor(TTree *tr, char *word1, char *word2) {
+    if (tr == NULL) return NULL;
+    if (strcmp(word1, tr->word) < 0 && strcmp(word2, tr->word) < 0)
+        return LowestCommonAncestor(tr->left, word1, word2);
+
+    if (strcmp(word1, tr->word) > 0 && strcmp(word2, tr->word) > 0)
+        return LowestCommonAncestor(tr->right, word1, word2);
+    return tr;
+}// here i calculate the length of word and compare it with renge 
+int CountNodesRanges(TTree *tr, int l, int h) {
+	int count,val;
+    if (tr == NULL)
+	return 0;
+    val = strlen(tr->word);
+    count = 0;
+    if (val >= l && val <= h)
+        count = 1;
+    return count + CountNodesRanges(tr->left, l, h) + CountNodesRanges(tr->right, l, h);
+}
+TTree* minValueNode(TTree* node) {
+    TTree* current = node;
+    while (current && current->left != NULL)
+        current = current->left;
+    return current;
+}
+
+TTree* inOrderSuccesor(TTree *root, char *word) {
+    TTree* successor = NULL;
+    while (root != NULL) {
+        if (strcmp(word, root->word) < 0) {
+            successor = root;
+            root = root->left;
+        } else {
+            root = root->right;}}
+    return successor;
+}
+
+bool isBalencedBST(TTree *tr){
+	if(tr==NULL)
+	return(true);
+	if(abs(Height(tr->left)-Height(tr->right))>1)
+	return(false);
+return(isBalencedBST(tr->left)&&isBalencedBST(tr->right));}
+
+void inOrderToList(TTree* root, TList* listTail) {
+    if (root == NULL) return;
+    inOrderToList(root->left, listTail);
+    TList newNode = (TList)malloc(sizeof(node));
+    strcpy(newNode->word, root->word);
+    newNode->next = NULL;
+    (*listTail)->next = newNode;
+    *listTail = newNode;
+    inOrderToList(root->right, listTail);
+}
+
+TList treeToList(TTree* root) {
+    TList list = (TList)malloc(sizeof(node));
+    list->next = NULL;
+    TList tail = list;
+    inOrderToList(root, &tail);
+    TList head = list->next;
+    free(list);
+    return head;
+}
+TList mergeLists(TList l1, TList l2) {
+    if (!l1) return l2;
+    if (!l2) return l1;
+
+    TList head = l1;
+    while (l1->next != NULL)
+        l1 = l1->next;
+    l1->next = l2;
+    return head;
 }
 
 
+TTree* BSTMerge(TTree* tr1, TTree* tr2) {
+	TList list1,list2,merged,sorted;
+	TTree *newTree;
+    list1 = treeToList(tr1);
+    list2 = treeToList(tr2);
+    merged = mergeLists(list1, list2);
+    sorted = sortWord(merged);
+     newTree = NULL;
+    while (sorted != NULL) {
+        newTree = AddWordBSTT(newTree, sorted->word,NULL,NULL);
+        sorted = sorted->next;
+    }
 
-
-
-
-
-
-
-
-
-
-
-void PrintLogEntries(logentry *head) {
-    logentry *temp =head;
-    while (temp != NULL) {
-        printf("ID: %d, Severity: %d, Message: %s, Date: %s\n", temp->id, temp->severity, temp->message, temp->date);
-        temp=temp->next;}}
-
-int main() {
-	logentry *head = NULL, *tail = NULL,*temp;
-	int arr[5]={1,2,3,4,5};
-
-    // Create 5 log entries with different timestamps
-    logentry *log1 = createLogEntry(7, 3, "System started", "2099-09-30 09:00:15");
-    logentry *log2 = createLogEntry(2, 20, "Warning: High CPU usage", "2025-10-29 09:05:42");
-    logentry *log3 = createLogEntry(3, 1, "Error: Disk space low", "2020-01-30 08:30:30");
-    logentry *log4 = createLogEntry(4, 4, "Security Alert: Unauthorized login", "2029-01-18 08:31:10");
-    logentry *log5 = createLogEntry(5, -1, "System shutdown initiated", "2101-01-29 09:50:05");
-
-    // Link nodes in a doubly linked list
-    head = log1;
-    log1->prv=NULL;
-    log1->next = log2;
-    log2->prv = log1;
-    
-    log2->next = log3;
-    log3->prv = log2;
-    
-    log3->next = log4;
-    log4->prv = log3;
-    
-    log4->next = log5;
-    log5->prv = log4;
-    log5->next=NULL;
-    tail = log5;  
-
-   
-    printf("Log Entries:\n");
-  
-      printf("%d",binary_serch(arr,0,4,5));
-        PrintLogEntries(temp);
-      
-    
-
-    
-    
-
-    return 0;
+    return newTree;
 }
+
