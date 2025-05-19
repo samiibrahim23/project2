eff2d250944ee653ec867e2c6af9201556d6bd9f
samiibrahim23 <khelifsami35@gmail.com>
2025-05-19 14:41:10 +0100

    Update file_rec.c

--- file_rec.c
+++ file_rec.c
@@ -60,5 +60,45 @@ void wordPermutation(char *word) {
     permute(word, 0, len - 1);
 }
 
+void findsubseq(char *word, char *subseq, int i, int j){
+    if (word[i] == '\0') {
+        subseq[j] = '\0';
+        printf("%s\n", subseq);
+        return;
+    }
+    subseq[j] = word[i];
+    findSubseq(word, subseq, i + 1, j + 1);
+    findSubseq(word, subseq, i + 1, j);
+}
+
+void subseqWord(char *word){
+    int leng=strlen(word);
+    char subseq=malloc(sizeof(char)*(leng+1));
+    findsubseq(word, subseq, 0, 0);
+}
+int longestSubseq(char *word1,char *word2,int leng1,int leng2){
+    if (leng1 == 0 || leng2 == 0)
+        return 0;
+    if (word1[leng1 - 1] == word2[leng2 - 1])
+        return 1 + longestSubseq(word1, word2, leng1 - 1, leng1 - 1);
+    else
+        return(longestSubseq(word1, word2, leng1 - 1, leng2)>   //return largest between them
+                longestSubseq(word1, word2, leng1, leng2 - 1))
+                ? longestSubseq(word1, word2, leng1 - 1, leng2)
+                : longestSubseq(word1, word2, leng1, leng2 - 1);
+}
+
+bool isPalind(char *word,int start,int end){
+    if (start >= end)  
+        return true;
+    if (word[start] != word[end])  
+        return false;
+    return isPalind(word,start+1,end-1);
+}
+
+bool isPalindromWord(char *word){
+    int lengt = strlen(word);
+    return isPalindromeHelper(word,0,lengt-1);
+}
 
 
